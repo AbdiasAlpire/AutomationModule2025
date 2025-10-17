7d273fce0f687e5f9f95d04764a3b10d724effdf
AbdiasAlpire <abdias.alpire@assuresoft.com>
2025-10-17 07:24:42 -0400

    fix: naming file

--- src/tests/frontend/login.spec.ts
+++ src/tests/frontend/login.spec.ts
@@ -1,16 +1,10 @@
-// src/tests/ui/login.spec.ts
-
 import { test, expect } from '@playwright/test';
 import LoginPageActions from '../../pom/actions/examplePage';
 
 test('Login fallido muestra mensaje de error', async ({ page }) => {
   const login = new LoginPageActions(page);
-
-  // Usando baseURL del config + path relativo
   await page.goto('/admin');
-
   await login.login('wronguser', 'wrongpass');
-
   const error = page.locator('#error-message');
   await expect(error).toHaveText('Invalid username or password');
 });
